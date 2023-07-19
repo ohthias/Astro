82742222a85a7495d878af4119eeb3efa3f62883
Matheus Gabriel <math.gsilva07@gmail.com>
2023-07-19 15:50:52 -0300

    add: create Music Player test

---
+++ Tests/Musicplayer/songs.js
@@ -0,0 +1,36 @@
+export default [
+  {
+    src: "./songs/VIva.wav",
+    name: "Viva",
+    artist: "Matheus",
+    img: "https://source.unsplash.com/featured/200x200",
+  },
+
+  {
+    src: "./songs/Reflexão.wav",
+    name: "Reflexão",
+    artist: "Matheus",
+    img: "https://source.unsplash.com/featured/199x200",
+  },
+
+  {
+    src: "http://physical-authority.surge.sh/music/1.mp3",
+    name: "No Time",
+    artist: "Lastlings",
+    img: "https://source.unsplash.com/featured/200x199",
+  },
+
+  {
+    src: "http://physical-authority.surge.sh/music/2.mp3",
+    name: "blinding lights",
+    artist: "the weekend",
+    img: "https://source.unsplash.com/featured/201x200",
+  },
+
+  {
+    src: "http://physical-authority.surge.sh/music/3.mp3",
+    name: "джованна",
+    artist: "enrasta",
+    img: "https://source.unsplash.com/featured/200x201",
+  },
+];
