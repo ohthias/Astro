3e74cc9c6f481b542255fb842237782c2ba9a0ab
Matheus Gabriel <math.gsilva07@gmail.com>
2023-07-19 15:52:29 -0300

    fix: redirect.js

--- Astro/Frontend/JS/redirect.js
+++ Astro/Frontend/JS/redirect.js
@@ -1,14 +1,19 @@
 var botaocadastro = document.getElementById('cadastrar')
 botaocadastro.addEventListener("click", function() {
     window.location.href = "/Astro/Frontend/Html/Login/sign_in.html"
-})
+});
 
 var botaoLogin = document.getElementById('entrar')
 botaoLogin.addEventListener("click", function() {
     window.location.href = "/Astro/Frontend/Html/Login/sign_up.html"
-})
+});
 
 var botaoHome = document.getElementById('buttonHome')
 botaoHome.addEventListener("click", function() {
     window.location.href = "/Astro/Frontend/Html/Base/home.html"
-})
+});
+
+var botaoOtherSongs = document.getElementById("othersSongs")
+botaoOtherSongs.addEventListener("click", function() {
+    window.location.href = "/Astro/Frontend/Html/Erro/comingSoon.html"
+});
