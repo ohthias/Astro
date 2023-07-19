7d5920c7d35b35a355deb1a5aa05f608b44b21d9
Matheus Gabriel <math.gsilva07@gmail.com>
2023-07-19 15:53:37 -0300

    add: 'music bank'

---
+++ Astro/Backend/JS/songs.js
@@ -0,0 +1,44 @@
+export default [
+    {
+      src: "http://physical-authority.surge.sh/music/6.mp3",
+      name: "",
+      artist: "",
+    },
+  
+    {
+      src: "http://physical-authority.surge.sh/music/5.mp3",
+      name: "",
+      artist: "",
+    },
+  
+    {
+      src: "http://physical-authority.surge.sh/music/1.mp3",
+      name: "No Time",
+      artist: "Lastlings",
+    },
+  
+    {
+      src: "http://physical-authority.surge.sh/music/2.mp3",
+      name: "blinding lights",
+      artist: "the weekend",
+    },
+  
+    {
+      src: "http://physical-authority.surge.sh/music/3.mp3",
+      name: "джованна",
+      artist: "enrasta",
+    },
+
+    {
+      src: "http://physical-authority.surge.sh/music/4.mp3",
+      name: "джованна",
+      artist: "enrasta",
+    },
+
+    {
+      src: "http://physical-authority.surge.sh/music/7.mp3",
+      name: "джованна",
+      artist: "enrasta",
+    },
+  ];
+  
