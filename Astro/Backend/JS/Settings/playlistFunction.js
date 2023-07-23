6186b567a1180e848af0dfcb8c436032f2d594e6
Matheus Gabriel <math.gsilva07@gmail.com>
2023-07-23 16:01:36 -0300

    aa0303

--- Astro/Backend/JS/Settings/playlistFunction.js
+++ Astro/Backend/JS/Settings/playlistFunction.js
@@ -1,17 +1,78 @@
 const musicas = [
   {
-    nome: "Beyond",
-    src: "/Assets/Music/Beyond.mp3",
-    artista: "Achour",
-    capaAlbum: "/Astro/Backend/JS/ImageAudio/2.jpg",
+    nome: "blinding lights",
+    src: "http://physical-authority.surge.sh/music/2.mp3",
+    artista: "the weekend",
+    capaAlbum: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcz6vl7dthSyfdNh6Eg4ZrcAYSieDSn7yCmQ&usqp=CAU",
+  },
+
+  {
+    src: "http://physical-authority.surge.sh/music/1.mp3",
+    nome: "No Time",
+    artista: "Lastlings",
+    capaAlbum:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBuflyiNrBSP0o-7KM9YqUsAZN8HIx7VCExFerUh4FMg&s",
   },
   {
-    nome: "Nome da Música 2",
-    src: "/Astro/Backend/JS/audio/10.mp3",
-    artista: "Artista 2",
-    capaAlbum: "",
+    src: "/Astro/Backend/JS/audio/1.mp3",
+    nome: "On My Way",
+    artista: "Alan Walker",
+    capaAlbum: "/Astro/Backend/JS/ImageAudio/1.jpg",
   },
-  // Adicione mais objetos de música aqui, se desejar
+
+  {
+    src: "/Astro/Backend/JS/audio/2.mp3",
+    nome: "Faded",
+    artista: "Alan Walker",
+    capaAlbum: "/Astro/Backend/JS/ImageAudio/2.jpg",
+  },
+
+  {
+    src: "/Astro/Backend/JS/audio/3.mp3",
+    nome: "On and On",
+    artista: "Pagali World",
+    capaAlbum: "/Astro/Backend/JS/ImageAudio/3.jpg",
+  },
+
+  {
+    src: "/Astro/Backend/JS/audio/13.mp3",
+    nome: "Baarishein",
+    artista: "Atif Aslam",
+    capaAlbum:
+      "/Astro/Backend/JS/ImageAudio/AlbumArt_{B5020207-474E-4720-7BF7-351BB9943900}_Small.jpg",
+  },
+
+  {
+    src: "/Astro/Backend/JS/audio/12.mp3",
+    nome: "Putt Jatt Da",
+    artista: "Diljit Dosanjh",
+    capaAlbum:
+      "/Astro/Backend/JS/ImageAudio/AlbumArt_{B5020207-474E-4720-E590-731D38E15100}_Small.jpg",
+  },
+
+  {
+    src: "/Astro/Backend/JS/audio/11.mp3",
+    nome: "Lagdi Lahore",
+    artista: "Guru Randhawa",
+    capaAlbum:
+      "/Astro/Backend/JS/ImageAudio/AlbumArt_{B5020207-474E-4720-63F6-EE1526A50100}_Small.jpg",
+  },
+
+  {
+    src: "/Astro/Backend/JS/audio/7.mp3",
+    nome: "Agar Tum Saath",
+    artista: "Alka Yagnik",
+    capaAlbum:
+      "/Astro/Backend/JS/ImageAudio/AlbumArt_{B5020207-474E-4720-8980-A9187807E300}_Small.jpg",
+  },
+
+  {
+    src: "/Astro/Backend/JS/audio/8.mp3",
+    nome: "Suna Hai",
+    artista: "Pagla",
+    capaAlbum: "/Astro/Backend/JS/ImageAudio/8.jpg",
+  },
+
 ];
 
 let isPlaying = false;
@@ -23,14 +84,27 @@ function exibirMusicas() {
   musicas.forEach((musica, index) => {
     const listItem = document.createElement("li");
 
+    const albumContainer = document.createElement("div");
+    albumContainer.classList.add("album-container");
+
     const capaAlbum = document.createElement("img");
     capaAlbum.src = musica.capaAlbum;
-    listItem.appendChild(capaAlbum);
+
+    const playIcon = document.createElement("i");
+    playIcon.classList.add("bx", "bx-caret-right", "play-icon");
+
+    albumContainer.appendChild(capaAlbum);
+    albumContainer.appendChild(playIcon);
+
+    albumContainer.addEventListener("click", () => {
+      tocarMusica(index);
+    });
+
+    listItem.appendChild(albumContainer);
 
     const infoMusica = document.createElement("div");
-    infoMusica.innerHTML = `<strong>${musica.nome}</strong><br>
-                                    Artista: ${musica.artista}<br>
-                                    <button onclick="tocarMusica(${index})">Tocar</button>`;
+    infoMusica.innerHTML = `<div class='infoMusica'><div class='infoMusica_play'><strong class='musicnome_playlist'>${musica.nome}</strong><br>
+                            <p class='nomeArtista_playlist'>${musica.artista}</p></div>`;
     listItem.appendChild(infoMusica);
 
     musicList.appendChild(listItem);
