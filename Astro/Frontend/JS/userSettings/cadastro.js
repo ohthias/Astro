4e8e0d06c4641024702cd8ce66137705b52e3b1b
Vinícius Alves e Sousa <math.gsilva07@gmail.com>
2023-07-26 10:28:59 -0300

    aa0511

--- Astro/Frontend/JS/userSettings/cadastro.js
+++ Astro/Frontend/JS/userSettings/cadastro.js
@@ -16,7 +16,7 @@ function cadastrarUsuario(nome, email, senha) {
   localStorage.setItem(nome, JSON.stringify(usuario));
 
   console.log('Usuário cadastrado com sucesso!');
-  window.location.href = 'sign_up.html';
+  window.location.href = '/Astro/Frontend/Html/Login/sign_up.html';
 }
 
 // Captura o formulário de cadastro
