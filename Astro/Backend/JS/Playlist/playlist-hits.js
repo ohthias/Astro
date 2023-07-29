d47ec5116d59efffc52fdce2e8858e2ae4cbc379
Matheus Gabriel <math.gsilva07@gmail.com>
2023-07-29 13:55:28 -0300

    aa0704

--- Astro/Backend/JS/Playlist/playlist-hits.js
+++ Astro/Backend/JS/Playlist/playlist-hits.js
@@ -1,76 +1,76 @@
 const musicas = [
-    {
-      nome: "blinding lights",
-      src: "http://physical-authority.surge.sh/music/2.mp3",
-      artista: "the weekend",
-      capaAlbum: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcz6vl7dthSyfdNh6Eg4ZrcAYSieDSn7yCmQ&usqp=CAU",
-    },
-  
-    {
-      src: "http://physical-authority.surge.sh/music/1.mp3",
-      nome: "No Time",
-      artista: "Lastlings",
-      capaAlbum:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBuflyiNrBSP0o-7KM9YqUsAZN8HIx7VCExFerUh4FMg&s",
-    },
-    {
-      src: "/Astro/Backend/JS/audio/1.mp3",
-      nome: "On My Way",
-      artista: "Alan Walker",
-      capaAlbum: "/Astro/Backend/JS/ImageAudio/1.jpg",
-    },
-  
-    {
-      src: "/Astro/Backend/JS/audio/2.mp3",
-      nome: "Faded",
-      artista: "Alan Walker",
-      capaAlbum: "/Astro/Backend/JS/ImageAudio/2.jpg",
-    },
-  
-    {
-      src: "/Astro/Backend/JS/audio/3.mp3",
-      nome: "On and On",
-      artista: "Pagali World",
-      capaAlbum: "/Astro/Backend/JS/ImageAudio/3.jpg",
-    },
-  
-    {
-      src: "/Astro/Backend/JS/audio/13.mp3",
-      nome: "Baarishein",
-      artista: "Atif Aslam",
-      capaAlbum:
-        "/Astro/Backend/JS/ImageAudio/AlbumArt_{B5020207-474E-4720-7BF7-351BB9943900}_Small.jpg",
-    },
-  
-    {
-      src: "/Astro/Backend/JS/audio/12.mp3",
-      nome: "Putt Jatt Da",
-      artista: "Diljit Dosanjh",
-      capaAlbum:
-        "/Astro/Backend/JS/ImageAudio/AlbumArt_{B5020207-474E-4720-E590-731D38E15100}_Small.jpg",
-    },
-  
-    {
-      src: "/Astro/Backend/JS/audio/11.mp3",
-      nome: "Lagdi Lahore",
-      artista: "Guru Randhawa",
-      capaAlbum:
-        "/Astro/Backend/JS/ImageAudio/AlbumArt_{B5020207-474E-4720-63F6-EE1526A50100}_Small.jpg",
-    },
-  
-    {
-      src: "/Astro/Backend/JS/audio/7.mp3",
-      nome: "Agar Tum Saath",
-      artista: "Alka Yagnik",
-      capaAlbum:
-        "/Astro/Backend/JS/ImageAudio/AlbumArt_{B5020207-474E-4720-8980-A9187807E300}_Small.jpg",
-    },
-  
-    {
-      src: "/Astro/Backend/JS/audio/8.mp3",
-      nome: "Suna Hai",
-      artista: "Pagla",
-      capaAlbum: "/Astro/Backend/JS/ImageAudio/8.jpg",
-    },
-  
-  ];
+  {
+    nome: "blinding lights",
+    src: "http://physical-authority.surge.sh/music/2.mp3",
+    artista: "the weekend",
+    capaAlbum:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcz6vl7dthSyfdNh6Eg4ZrcAYSieDSn7yCmQ&usqp=CAU",
+  },
+
+  {
+    src: "http://physical-authority.surge.sh/music/1.mp3",
+    nome: "No Time",
+    artista: "Lastlings",
+    capaAlbum:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBuflyiNrBSP0o-7KM9YqUsAZN8HIx7VCExFerUh4FMg&s",
+  },
+  {
+    src: "/Astro/Backend/JS/audio/1.mp3",
+    nome: "On My Way",
+    artista: "Alan Walker",
+    capaAlbum: "/Astro/Backend/JS/ImageAudio/1.jpg",
+  },
+
+  {
+    src: "/Astro/Backend/JS/audio/2.mp3",
+    nome: "Faded",
+    artista: "Alan Walker",
+    capaAlbum: "/Astro/Backend/JS/ImageAudio/2.jpg",
+  },
+
+  {
+    src: "/Astro/Backend/JS/audio/3.mp3",
+    nome: "On and On",
+    artista: "Pagali World",
+    capaAlbum: "/Astro/Backend/JS/ImageAudio/3.jpg",
+  },
+
+  {
+    src: "/Astro/Backend/JS/audio/13.mp3",
+    nome: "Baarishein",
+    artista: "Atif Aslam",
+    capaAlbum:
+      "/Astro/Backend/JS/ImageAudio/AlbumArt_{B5020207-474E-4720-7BF7-351BB9943900}_Small.jpg",
+  },
+
+  {
+    src: "/Astro/Backend/JS/audio/12.mp3",
+    nome: "Putt Jatt Da",
+    artista: "Diljit Dosanjh",
+    capaAlbum:
+      "/Astro/Backend/JS/ImageAudio/AlbumArt_{B5020207-474E-4720-E590-731D38E15100}_Small.jpg",
+  },
+
+  {
+    src: "/Astro/Backend/JS/audio/11.mp3",
+    nome: "Lagdi Lahore",
+    artista: "Guru Randhawa",
+    capaAlbum:
+      "/Astro/Backend/JS/ImageAudio/AlbumArt_{B5020207-474E-4720-63F6-EE1526A50100}_Small.jpg",
+  },
+
+  {
+    src: "/Astro/Backend/JS/audio/7.mp3",
+    nome: "Agar Tum Saath",
+    artista: "Alka Yagnik",
+    capaAlbum:
+      "/Astro/Backend/JS/ImageAudio/AlbumArt_{B5020207-474E-4720-8980-A9187807E300}_Small.jpg",
+  },
+
+  {
+    src: "/Astro/Backend/JS/audio/8.mp3",
+    nome: "Suna Hai",
+    artista: "Pagla",
+    capaAlbum: "/Astro/Backend/JS/ImageAudio/8.jpg",
+  },
+];
