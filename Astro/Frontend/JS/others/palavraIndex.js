a99863557c77b056650af868d0ec90a80484dff1
Matheus Gabriel <math.gsilva07@gmail.com>
2023-08-05 15:36:53 -0300

    aa0901

--- Astro/Frontend/JS/others/palavraIndex.js
+++
@@ -1,9 +0,0 @@
-const palavras = ['no carro', 'no ônibus', 'nas galaxias', 'em casa' ];
-let indice = 0;
-
-const spanElement = document.getElementById('palavra');
-
-setInterval(() => {
-  spanElement.innerText = palavras[indice];
-  indice = (indice + 1) % palavras.length;
-}, 2500);
