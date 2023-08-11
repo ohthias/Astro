3fa081e4115be97e88c5c86ad012a2af555c04c9
Matheus Gabriel <math.gsilva07@gmail.com>
2023-08-11 10:17:31 -0300

    aa0910

--- Astro/Backend/JS/ArtistsPage/artistspage.js
+++ Astro/Backend/JS/ArtistsPage/artistspage.js
@@ -1,18 +1,52 @@
-const artistpage = [
-    {
-        id: 1,
-        name: "Artista 1",
-        about: "Descrição do Artista 1.",
-        follows: "1000",
-        ouvintes: "50000",
-      },
-      {
-        id: 2,
-        name: "Artista 2",
-        about: "Descrição do Artista 2.",
-        follows: "2000",
-        ouvintes: "75000",
-      },
-]
+const artistpage = {
+  id001: {
+    nameArtist: "",
+    about: "Descrição do Artista 1.",
+    follows: "1000",
+    ouvintes: "50000",
+  },
+  id002: {
+    nameArtist: "",
+    about: "Descrição do Artista 1.",
+    follows: "1000",
+    ouvintes: "50000",
+  },
+  id003: {
+    nameArtist: "",
+    about: "Descrição do Artista 1.",
+    follows: "1000",
+    ouvintes: "50000",
+  },
+  id004: {
+    nameArtist: "",
+    about: "Descrição do Artista 1.",
+    follows: "1000",
+    ouvintes: "50000",
+  },
+  id005: {
+    nameArtist: "",
+    about: "Descrição do Artista 1.",
+    follows: "1000",
+    ouvintes: "50000",
+  },
+  id006: {
+    nameArtist: "",
+    about: "Descrição do Artista 1.",
+    follows: "1000",
+    ouvintes: "50000",
+  },
+  id007: {
+    nameArtist: "",
+    about: "Descrição do Artista 1.",
+    follows: "1000",
+    ouvintes: "50000",
+  },
+  id008: {
+    nameArtist: "",
+    about: "Descrição do Artista 1.",
+    follows: "1000",
+    ouvintes: "50000",
+  },
+};
 
-export default artistpage
+export default artistpage;
