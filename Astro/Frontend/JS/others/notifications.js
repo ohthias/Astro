c5add7175376b5e142f133a3bf71d8669fcbf760
Matheus Gabriel <math.gsilva07@gmail.com>
2023-08-11 09:47:54 -0300

    aa0908

---
+++ Astro/Frontend/JS/others/notifications.js
@@ -0,0 +1,36 @@
+const notifications = document.querySelector(".notifications"),
+buttons = document.querySelectorAll(".buttons .btn");
+
+// Object containing details for different types of toasts
+const toastDetails = {
+    timer: 5000,
+    success: {
+        icon: 'fi-br-waveform-path',
+        text: 'O Astro ainda está em desenvolvimento.',
+    },
+
+}
+
+const removeToast = (toast) => {
+    toast.classList.add("hide");
+    if(toast.timeoutId) clearTimeout(toast.timeoutId); // Clearing the timeout for the toast
+    setTimeout(() => toast.remove(), 500); // Removing the toast after 500ms
+}
+
+const createToast = (id) => {
+    // Getting the icon and text for the toast based on the id passed
+    const { icon, text } = toastDetails[id];
+    const toast = document.createElement("li"); // Creating a new 'li' element for the toast
+    toast.className = `toast ${id}`; // Setting the classes for the toast
+    toast.innerHTML = `<div class="column">
+                         <i class="fa-solid ${icon}"></i>
+                         <span>${text}</span>
+                      </div>
+                      <i class="fa-solid fa-xmark" onclick="removeToast(this.parentElement)"></i>`;
+    notifications.appendChild(toast); // Append the toast to the notification ul
+    toast.timeoutId = setTimeout(() => removeToast(toast), toastDetails.timer);
+}
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => createToast(btn.id));
+});
