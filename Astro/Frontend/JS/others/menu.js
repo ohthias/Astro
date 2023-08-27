71cd68d8654655733016e5afdbf0d8d3d400f1bd
Matheus Gabriel <math.gsilva07@gmail.com>
2023-08-27 19:38:16 -0300

    update at0103

--- Astro/Frontend/JS/others/menu.js
+++ Astro/Frontend/JS/others/menu.js
@@ -2,25 +2,22 @@ let sidebar = document.querySelector(".sidebar");
 let closeBtn = document.querySelector("#btn");
 let searchBtn = document.querySelector(".bx-search");
 
-// Removemos o event listener do botão de fechar
-// closeBtn.addEventListener("click", () => {
-//   sidebar.classList.toggle("close");
-//   menuBtnChange();
-// });
-
-// Inicialmente, a barra lateral estará aberta
-sidebar.classList.toggle("open");
-menuBtnChange();
-
-searchBtn.addEventListener("click", () => {
-  sidebar.classList.remove("open");
-  menuBtnChange();
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("open");
+  menuBtnChange(); //calling the function(optional)
 });
 
+searchBtn.addEventListener("click", () => {
+  // Sidebar open when you click on the search iocn
+  sidebar.classList.toggle("open");
+  menuBtnChange(); //calling the function(optional)
+});
+
+// following are the code to change sidebar button(optional)
 function menuBtnChange() {
   if (sidebar.classList.contains("open")) {
-    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
   } else {
-    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
   }
 }
