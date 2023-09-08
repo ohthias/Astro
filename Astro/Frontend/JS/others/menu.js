a3d6e135dbe2c440d19c07e58ea3f8212b3e52e1
Matheus Gabriel <math.gsilva07@gmail.com>
2023-09-08 14:25:32 -0300

    fix login and menu

--- Astro/Frontend/JS/others/menu.js
+++ Astro/Frontend/JS/others/menu.js
@@ -1,17 +1,5 @@
 let sidebar = document.querySelector(".sidebar");
 let closeBtn = document.querySelector("#btn");
-let searchBtn = document.querySelector(".bx-search");
-
-closeBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-  menuBtnChange(); //calling the function(optional)
-});
-
-searchBtn.addEventListener("click", () => {
-  // Sidebar open when you click on the search iocn
-  sidebar.classList.toggle("open");
-  menuBtnChange(); //calling the function(optional)
-});
 
 // following are the code to change sidebar button(optional)
 function menuBtnChange() {
@@ -21,3 +9,9 @@ function menuBtnChange() {
     closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
   }
 }
+
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("open");
+  menuBtnChange(); //calling the function(optional)
+});
+
