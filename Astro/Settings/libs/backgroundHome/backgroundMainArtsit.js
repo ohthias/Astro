f10f9c5affa40b9093594d76dddccde15f09f2a2
Matheus Gabriel <math.gsilva07@gmail.com>
2023-10-08 19:29:45 -0300

    fix - player.css

--- Astro/Settings/libs/backgroundHome/backgroundMainArtsit.js
+++ Astro/Settings/libs/backgroundHome/backgroundMainArtsit.js
@@ -1,17 +1,37 @@
 const backArtist = [
   {
-    src: "https://robomagiclive.com/wp-content/uploads/2020/11/Neffex-min.jpg",
+    src: "https://akamai.sscdn.co/uploadfile/letras/fotos/2/7/c/b/27cb4e296f3008772304f6b928b05e54.jpg",
     nameArtist: "NEFFEX",
   },
 
   {
-    src: "https://media.tracktribe.com/images/Social_Share_img.jpg",
+    src: "https://i.ytimg.com/vi/DE8B3Ds5Kjs/maxresdefault.jpg",
     nameArtist: "TrackTribe",
   },
 
   {
-    src: "https://i.ytimg.com/vi/T6YPsaBIcC0/maxresdefault.jpg",
+    src: "https://cdn.audiolibrary.com.co/themes/ytalc/assets/img/songs/deux-by-dyalla.jpg",
     nameArtist: "Dyalla",
+  },
+
+  {
+    src: "https://i.ytimg.com/vi/U-uqWtglbCo/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAE-DQzaGPH-vIxpqwTcM8PAtpcgA",
+    nameArtist: "Otis McDonald",
+  },
+
+  {
+    src: "https://media.pitchfork.com/photos/5f6b50a5ffb0eebbb428ce53/2:1/w_2560%2Cc_limit/Emily-A-Sprague.jpg",
+    nameArtist: "Emily Sprague",
+  },
+  
+  {
+    src: "https://d10j3mvrs1suex.cloudfront.net/s:bzglfiles/u/201590/eb7510f4db0180e52ff1582eed2b1afcfca1793d/original/doug-wood-promo-updated-levels.jpg/!!/b%3AW1sicmVzaXplIiwxODAwXSxbIm1heCJdLFsid2UiXV0%3D/meta%3AeyJzcmNCdWNrZXQiOiJiemdsZmlsZXMifQ%3D%3D.jpg",
+    nameArtist: "Dougie Wood",
+  },
+
+  {
+    src: "https://deepcreektimes.com/wp-content/uploads/Jason-Shaw-Music2-1024x1024.jpg",
+    nameArtist: "Audionautix",
   },
 ];
 
