80a2a3f59e6f00b322fd25cf315cd9ff688f2f25
Matheus Gabriel <math.gsilva07@gmail.com>
2023-10-10 21:13:41 -0300

    fix - mix function

--- Astro/Settings/libs/mixes/mixesRedirect.js
+++ Astro/Settings/libs/mixes/mixesRedirect.js
@@ -1,23 +1,11 @@
-document.getElementById("playlist1").addEventListener("click", function () {
-  // Redirecionar para a página de mixes com um parâmetro para indicar a playlist escolhida
-  window.location.href =
-    "/Astro/src/playlist/mixes/mixes.html?playlist=1";
-});
+// Loop para criar event listeners para os elementos de playlist1 a playlist8
+for (let i = 1; i <= 8; i++) {
+  const playlistElement = document.getElementById(`playlist${i}`);
 
-document.getElementById("playlist2").addEventListener("click", function () {
-  // Redirecionar para a página de mixes com um parâmetro para indicar a playlist escolhida
-  window.location.href =
-    "/Astro/src/playlist/mixes/mixes.html?playlist=2";
-});
-
-document.getElementById("playlist3").addEventListener("click", function () {
-  // Redirecionar para a página de mixes com um parâmetro para indicar a playlist escolhida
-  window.location.href =
-    "/Astro/src/playlist/mixes/mixes.html?playlist=3";
-});
-
-document.getElementById("playlist5").addEventListener("click", function () {
-  // Redirecionar para a página de mixes com um parâmetro para indicar a playlist escolhida
-  window.location.href =
-    "/Astro/src/playlist/mixes/mixes.html?playlist=4";
-});
+  if (playlistElement) {
+    playlistElement.addEventListener("click", function () {
+      // Redirecionar para a página de mixes com um parâmetro para indicar a playlist escolhida
+      window.location.href = `/Astro/src/playlist/mixes/mixes.html?playlist=${i}`;
+    });
+  }
+}
