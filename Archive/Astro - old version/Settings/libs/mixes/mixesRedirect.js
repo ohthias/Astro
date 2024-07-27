4cdf0c94b5e193d3ff2c1b08b2bc86f8a1ce754a
Matheus Gabriel <math.gsilva07@gmail.com>
2024-07-27 10:50:16 -0300

    delete old version

--- Archive/Astro - old version/Settings/libs/mixes/mixesRedirect.js
+++
@@ -1,9 +0,0 @@
-for (let i = 1; i <= 8; i++) {
-  const playlistElement = document.getElementById(`playlist${i}`);
-
-  if (playlistElement) {
-    playlistElement.addEventListener("click", function () {
-      window.location.href = `/Astro/src/playlist/mixes/mixes.html?playlist=${i}`;
-    });
-  }
-}
