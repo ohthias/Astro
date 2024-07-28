c0afbc5e8b19548a910d8eabeba4cb82bd48d9a8
Matheus Gabriel <math.gsilva07@gmail.com>
2024-07-28 16:53:09 -0300

    add navbar

--- Astro/src/App.jsx
+++ Astro/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./components/index/Navbar.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function IndexAstro() {
+  return <>{navBar()}</>;
 }
 
-export default App
+function navBar() {
+  return (
+    <>
+      <nav className="navgation_index">
+        <button className="navgation_index_button-logo bebas-neue">Astro</button>
+        <div className="navgation_index_container">
+          <a href="https://github.com/Matheus-Gabriel07/Astro" target="_blank" className="navegation_link bebas-neue">Documentação</a>
+          <a href="#" target="_blank" className="navegation_link bebas-neue">Sobre</a>
+          <button className="button_entrar bebas-neue">Entrar</button>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default IndexAstro;
